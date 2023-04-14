45e1cd027c407514753e35d0e0a7a40b03ba4338
marialapc <marialopez.apc@gmail.com>
2023-04-14 13:24:45 +0200

    improve layout

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 import "../App/App.scss";
 import StateSearcher from "../StateSearcher/StateSearcher";
+import america from "../App/america.png";
 
 function App() {
 
-  return (<StateSearcher />);
+  return (
+  <div className="main">
+  <img src={america} alt="america"  className="image"/>
+  <div className="container">
+  <h1 className="title">Choose your state</h1>
+  <StateSearcher />
+  </div>
+  </div>
+  );
 }
 
 export default App;
