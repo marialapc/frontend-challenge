cd39cafc0b226531f43ca450c7611a9d6aa7240a
marialapc <marialopez.apc@gmail.com>
2023-04-15 11:36:23 +0200

    chanche image rute

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import "../App/App.scss";
 import StateSearcher from "../StateSearcher/StateSearcher";
+import image from "../../assets/america.png"
 
 
 function App() {
   return (
     <div className="main">
-      <img src="/images/america.png" alt="america" className="image" />
+      <img src={image} alt="america" className="image" />
       <div className="container">
         <h1 className="title">Choose your state</h1>
         <StateSearcher />
